d2041190be6b2ac84ce8089ca7656542f3c71d09
Jackson Lorran <computadordb@gmail.com>
2023-10-25 17:36:08 -0300

    Adicioando alguns leet_codes

--- contagem_de_vogais.py
+++ contagem_de_vogais.py
@@ -1,6 +1,17 @@
-numero = 7  # Substitua 7 pelo número que você deseja verificar
+'''
+DESCRIPTION:
+Return the number (count) of vowels in the given string.
 
-if numero % 2 == 1:
-    print(f"{numero} é ímpar.")
-else:
-    print(f"{numero} não é ímpar.")
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+'''
+
+def get_count(sentence):
+    total_count= 0
+    for x in sentence:
+        if x == 'a' or x == 'e' or x == 'i'or  x == 'o' or x=='u':
+            total_count +=1
+
+    return total_count
+print(get_count("eu ja sei o que aconteceu"))
---
+++ digital_root.py
@@ -0,0 +1,26 @@
+'''DESCRIPTION:
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+'''
+
+def digital_root(n):
+    #transformando um inteiro em lista
+    lista = list(map(int, str(n)))
+        #somando os valores da lista
+    total = sum(lista)
+    while total > 9:
+        lista = list(map(int, str(total)))
+        total = sum(lista)
+    if total < 10:
+        return total
+
+    a = digital_root(493193)
+
+    print(a)
